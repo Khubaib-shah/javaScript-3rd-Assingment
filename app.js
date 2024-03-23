b8012bd83f6bf523aa0540ddec37901ad6748a26
khubaib shah <160419480+Khubaib-shah@users.noreply.github.com>
2024-03-23 08:29:23 +0500

    Array & Loop

---
+++ app.js
@@ -0,0 +1,62 @@
+// Q.1
+// let emptyArray = new Array(3);
+// for (let i = 0; i < emptyArray.length; i++) {
+//   emptyArray[i] = new Array(4);
+// }
+// console.log(emptyArray);
+
+// // Q.2
+// var metrixArry = [
+//   [0, 1, 2, 3],
+//   [1, 0, 1, 2],
+//   [2, 1, 0, 2],
+// ];
+// console.log(metrixArry);
+
+// // Q.3
+// for (var i = 1; i <= 10; i++) {
+//   document.write(i + "<br>");
+// }
+
+// Q.4
+// let num = prompt("Enter a number to show its multiplication table:");
+// let length = prompt("Enter length multiplication table:");
+// for (let i = 1; i <= length; i++) {
+//   let tabler = num * i;
+//   document.write(num + " x " + i + " = " + tabler + "<br>");
+// }
+
+
+// Q.5
+// i dont know how to do it 
+
+
+// Q.6
+// a
+// let countingArray = [];
+// for (let i = 1; i <= 15; i++) {
+//   countingArray.push(i);
+// }
+// document.write("Counting " + countingArray + "<br/>");
+// // b
+// let reverseArray = [];
+// for (let i = 10; i > 0; i--) {
+//   reverseArray.push(i);
+// }
+// document.write("Reverse Counting " + reverseArray + "<br/>"); 
+// // c
+// let evenArray = [];
+// for (let i = 0; i <= 20; i += 2) {
+//   evenArray.push(i);
+// }
+// document.write("Even " + evenArray + "<br/>");
+// // d
+// let series = "";
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 !== 0) {
+//     series += i + "k ";
+//   } else {
+//     series += i + "k ";
+//   }
+// }
+// document.write("Series: " + series + "<br>");
